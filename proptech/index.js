8e2454f71b5f487e2382236c5de89a86ed5e28f1
scott88lee <scott88lee@gmail.com>
2022-12-28 17:32:53 +0800

    balaConversion

--- proptech/index.js
+++ proptech/index.js
@@ -2,19 +2,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-//read json file, balaTable.json
 const fs = require('fs');
 const balaTable = JSON.parse(fs.readFileSync('balaTable.json', 'utf8'));
 
 for (var i = 0; i < balaTable.length; i++) {
-    console.log((99-i) + " - " + balaTable[i]);
+  console.log((99 - i) + " - " + balaTable[i]);
 }
 
-app.get('/balaTable', (req, res) => res.send(balaTable));
+app.get('/balaTable', (req, res) => {
+  console.log(req.query);
+  let { lease, price } = req.query;
+  let response = { propertyLease: lease, propertyPrice: price };
+
+  console.log(lease);
+  console.log(price);
+
+  if (lease && price) {
+    response.NewLaunch99Ratio = balaTable[0];
+    response.BalaRatio = balaTable[99 - lease];
+
+    response.FHNLPrice = price / balaTable[99 - lease];
+    response.NNNLPrice = (balaTable[6] / balaTable[99 - lease] * price).toFixed(2);
+  }
+
+  res.send(response);
+});
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    }
+  res.send('Hello World!')
+}
 );
 
 app.listen(port, () => console.log(`Proptech app listening on port ${port}!`));
